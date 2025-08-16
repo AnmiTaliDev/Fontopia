751903d1efa002c8af3b7eca76986ec71d9b91bb
AnmiTaliDev <153644302+AnmiTaliDev@users.noreply.github.com>
2025-08-16 16:31:45 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Fontopia/', // Замените 'fontopia' на название вашего репозитория
+  base: '/Fontopia/', 
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
+    minify: 'terser',
     rollupOptions: {
       output: {
         manualChunks: {
@@ -17,5 +18,9 @@ export default defineConfig({
         }
       }
     }
+  },
+  server: {
+    port: 3000,
+    open: true
   }
 })
